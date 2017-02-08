56813c62ca8196bc66c8379b863b2b9497efdaf6
ang-zy <angzy95@yahoo.com.sg>
2017-02-08 12:54:09 +0800

    Create Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Denotes that a class can be represented a printable string
+ *
+ */
+public interface Printable {
+    
+    /**
+     * Method to get printable string representation of the object
+     * @return printable string representation of the object
+     */
+    public String getPrintableString();
+    
+}
